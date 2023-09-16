94d294382239d78b32ce7614e4a98d4b766887aa
Nikita Levushkin <therederickmind@gmail.com>
2023-09-16 19:00:51 +0300

    Added opinion Form and changed templates for it, also changed template for homepage (full links and etc.)

--- opinions_app.py
+++ opinions_app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 # функция выбора случайного значения
 from random import randrange
+# Новые импорты
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
 
 app = Flask(__name__)
 
@@ -10,6 +15,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 # Задаётся конкретное значение для конфигурационного ключа
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'Royal with cheese'
 # В ORM передаётся в качестве параметра экземпляр приложения Flask
 db = SQLAlchemy(app)
 
@@ -20,6 +26,23 @@ class Opinion(db.Model):
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
+    )
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+    submit = SubmitField('Добавить')
 
 
 @app.route('/')
@@ -37,9 +60,35 @@ def index_view():
     return render_template('opinion.html', opinion=opinion)
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    return render_template('add_opinion.html')
+    form = OpinionForm()
+    # Если ошибок не возникло, то
+    if form.validate_on_submit():
+        # нужно создать новый экземпляр класса Opinion
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        # Затем добавить его в сессию работы с базой данных
+        db.session.add(opinion)
+        # И зафиксировать изменения
+        db.session.commit()
+        # Затем перейти на страницу добавленного мнения
+        return redirect(url_for('opinion_view', id=opinion.id))
+    # Иначе просто отрисовать страницу с формой
+    return render_template('add_opinion.html', form=form)
+
+
+# Тут указывается конвертер пути для id
+@app.route('/opinions/<int:id>')
+# Параметром указывается имя переменной
+def opinion_view(id):
+    # Теперь можно запрашивать мнение по id
+    opinion = Opinion.query.get_or_404(id)
+    # И передавать его в шаблон
+    return render_template('opinion.html', opinion=opinion)
 
 
 if __name__ == '__main__':
